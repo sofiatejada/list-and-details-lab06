bbcc4890979f4e0f92bb1c7cb16a1fb335a172e8
Sofia Tejada <sofianais@icloud.com>
2021-09-19 19:57:38 -0700

    fetch functions

--- src/services/heyArnoldApi.js
+++ src/services/heyArnoldApi.js
@@ -1,1 +1,15 @@
+const URL = 'https://hey-arnold-api.herokuapp.com/api/v1/characters';
 
+export const fetchAllCharacters = async () => {
+  const res = await fetch(URL);
+  const characters = await res.json();
+
+  return characters;
+};
+
+export const fetchOneCharacter = async (id) => {
+  const res = await fetch(`${URL}/${id}`);
+  const character = await res.json();
+
+  return character;
+};
