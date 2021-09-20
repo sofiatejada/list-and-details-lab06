e02f91d76255a9145948c26f5e52a33df1c18489
Sofia Tejada <sofianais@icloud.com>
2021-09-19 20:53:28 -0700

    app.jsx

---
+++ src/containers/HeyArnoldDetail.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import fetchCharacters from '../services/heyArnoldApi';
+import CharacterList from '../components/characters/CharacterList';
+import spinner from './heyarnoldspinner.gif';
+
+
---
+++ src/containers/HeyArnoldList.jsx
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import fetchCharacters from '../services/heyArnoldApi';
+import CharacterList from '../components/characters/CharacterList';
+import spinner from './heyarnoldspinner.gif';
