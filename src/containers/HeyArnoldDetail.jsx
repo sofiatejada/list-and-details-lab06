742b557f24b437bc3f60208fca829166f9be988f
Sofia Tejada <sofianais@icloud.com>
2021-09-19 21:20:25 -0700

    heyarnolddetail.jsx

--- src/containers/HeyArnoldDetail.jsx
+++ src/containers/HeyArnoldDetail.jsx
@@ -1,6 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import fetchCharacters from '../services/heyArnoldApi';
-import CharacterList from '../components/characters/CharacterList';
+import { fetchOneCharacter } from '../services/heyArnoldApi';
 import spinner from './heyarnoldspinner.gif';
+import { Link, useParams } from 'react-router-dom';
+import Character from '../components/characters/Character';
 
+const HeyArnoldDetail = () => {
 
+  const { id } = useParams();
+  const [character, setCharacter] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchOneCharacter(id)
+      .then((character) => setCharacter(character))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  return (
+    <main>
+      <header>
+        <Link to="/">
+          Homepage
+        </Link>
+      </header>
+      <content>
+        {loading ? 
+          <img src={spinner} alt="loading gif" /> : 
+          <Character {...character} />}
+      </content>
+    </main>
+  );
+};
+
+export default HeyArnoldDetail;
--- src/containers/HeyArnoldList.jsx
+++ src/containers/HeyArnoldList.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import fetchCharacters from '../services/heyArnoldApi';
+import { fetchAllCharacters } from '../services/heyArnoldApi';
 import CharacterList from '../components/characters/CharacterList';
 import spinner from './heyarnoldspinner.gif';
 
 const HeyArnoldList = () => {
 
-  const [characterList, setCharacterList] = useState(null);
+  const [characterList, setCharacterList] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchCharacters()
+    fetchAllCharacters()
       .then((characters) => setCharacterList(characters))
       .finally(() => setLoading(false));
   }, []);
