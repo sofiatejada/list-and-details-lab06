062fe7d7588fa0867d135dacfd72d2d25614b74a
Sofia Tejada <sofianais@icloud.com>
2021-09-19 21:06:21 -0700

    heyarnoldlist.jsx

--- src/containers/HeyArnoldList.jsx
+++ src/containers/HeyArnoldList.jsx
@@ -2,3 +2,25 @@ import React, { useState, useEffect } from 'react';
 import fetchCharacters from '../services/heyArnoldApi';
 import CharacterList from '../components/characters/CharacterList';
 import spinner from './heyarnoldspinner.gif';
+
+const HeyArnoldList = () => {
+
+  const [characterList, setCharacterList] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchCharacters()
+      .then((characters) => setCharacterList(characters))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if(loading) {
+    return (
+      <img src={spinner} alt="loading gif" />
+    );
+  }
+
+  return <CharacterList characters={characterList} />;
+};
+
+export default HeyArnoldList;
