483a008e523d47cea0027effcf15cb9df0fc5f37
Sofia Tejada <sofianais@icloud.com>
2021-09-19 21:48:55 -0700

    characterlist.jsx

--- src/components/characters/CharacterList.jsx
+++ src/components/characters/CharacterList.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Character from './Character';
 
-export default function CharacterList() {
+export default function CharacterList({ characters }) {
   return (
-    <div>
-      
-    </div>
+    <ul aria-label="character-list">
+      {characters.map((character) => (
+        <li key={character.id}>
+          <Link to="`/${character.id}`">
+            <Character {...character} />
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 }
+
+CharacterList.propTypes = {
+  characters: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+    })
+  )
+};
